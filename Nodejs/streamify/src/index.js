accef60e5bc80294cc85d07c508a4987e4de111e
fanendra5869 <fanendra.choudhary@kriotek.in>
2026-01-16 16:25:00 +0530

    Add some files to hnadle Api errors

--- Nodejs/streamify/src/index.js
+++ Nodejs/streamify/src/index.js
@@ -1,13 +1,32 @@
 // require("dotenv").config({ path: "./env" });
 import dotenv from "dotenv";
 import connectDB from "./db/db.js";
+import express from "express";
+
+const app = express();
 
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(
+        `Server is running on Port : http://localhost:${process.env.PORT || 8000}`
+      );
+    });
+  })
+  .catch((error) => {
+    console.error("Failed to connect to the database:", error);
+    process.exit(1);
+  });
 
+
+
+
+
+  
 // import express from "express";
 // const app = express();
 
