238425393d1f093d1b0a8e68c5fffd2e0c744a24
fanendra5869 <fanendra.choudhary@kriotek.in>
2026-01-23 12:14:52 +0530

    Add Cloudinary

--- Nodejs/streamify/src/models/user.model.js
+++ Nodejs/streamify/src/models/user.model.js
@@ -60,4 +60,31 @@ userSchema.methods.isPasswordCorrect = async function (password) {
   return await bcrypt.compare(password, this.password);
 };
 
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      username: this.username,
+      email: this.email,
+      fullName: this.fullname,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRES,
+    }
+  );
+};
+
+userSchema.methods.generateRefreshToken = function () {
+    return jwt.sign(
+      {
+        _id: this._id,
+      },
+      process.env.REFRESH_TOKEN_SECRET,
+      {
+        expiresIn: process.env.REFRESH_TOKEN_EXPIRES,
+      }
+    );
+};
+
 export const User = mongoose.model("User", userSchema);
---
+++ Nodejs/streamify/src/utils/Cloudinary.js
@@ -0,0 +1,26 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadToCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+
+    // upload the file on Cloudinary
+    const result = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+
+    // file hass been uploaded
+    console.log("File uploaded to Cloudinary successfully", result.url);
+    return result;
+  } catch (error) {
+    fs.unlinkSync(localFilePath); // remove the local saved temporary file as the upload operation got failed
+  }
+};
+
+export { uploadToCloudinary };
